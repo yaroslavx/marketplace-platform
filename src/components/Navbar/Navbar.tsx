c2599b11c0b4a1ae5cae6b106a7dd9c26b26fb13
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-04-05 18:06:01 +0300

    Install react-router and prepare navbar for animations

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
+          {/*<Link to="/">*/}
           <span className="text">MarketPlace</span>
+          {/*</Link>*/}
           <span className="dot">.</span>
         </div>
         <div className="links">
@@ -16,6 +19,11 @@ export const Navbar = () => {
           <span>Become a Seller</span>
           <button>Join</button>
         </div>
+      </div>
+      <hr />
+      <div className="menu">
+        <span>Test</span>
+        <span>Test1</span>
       </div>
     </div>
   );
