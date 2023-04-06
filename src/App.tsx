025f9ca6fab6f10c20fdc5b21a66c6a73e6176ac
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-04-06 11:14:14 +0300

    Implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,70 @@
 import React from "react";
-import { Navbar } from "src/components/Navbar/Navbar";
-import "./app.scss";
+import { Navbar } from "src/components/Navbar";
+import "src/app.scss";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { HomePage } from "src/pages/HomePage";
+import { Footer } from "src/components/Footer";
+import { GigsPage } from "src/pages/GigsPage";
+import { GigPage } from "src/pages/GigPage";
+import { OrdersPage } from "src/pages/OrdersPage";
+import { MyGigsPage } from "src/pages/MyGigsPage";
+import { AddPage } from "src/pages/AddPage";
+import { MessagesPage } from "src/pages/MessagesPage";
+import { MessagePage } from "src/pages/MessagePage";
 
 export const App = () => {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/gigs",
+          element: <GigsPage />,
+        },
+        {
+          path: "/gigs/:id",
+          element: <GigPage />,
+        },
+        {
+          path: "/orders",
+          element: <OrdersPage />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigsPage />,
+        },
+        {
+          path: "/add",
+          element: <AddPage />,
+        },
+        {
+          path: "/messages",
+          element: <MessagesPage />,
+        },
+        {
+          path: "/messages/:id",
+          element: <MessagePage />,
+        },
+      ],
+    },
+  ]);
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
     </div>
   );
 };
